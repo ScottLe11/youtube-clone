5e27cabc90932cd1b2cb66c4970a3064b62e8a08
Scott Le <60492217+ScottLe11@users.noreply.github.com>
2026-09-08 17:30:37 -0700

    created simple express starting app

---
+++ video-processing-service/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+const app = express();
+
+// default port for express
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+
+});
+
+app.listen(port, () => {
+    console.log(`video processing service listening on http://localhost:${port}`);
+
+});
